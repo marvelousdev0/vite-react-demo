56ee6c0df0bdcbd8165815dff82c3afd977ed672
Vamshi Maddur <maddur.vamshi@gmail.com>
2021-04-28 04:26:35 -0400

    header init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import Modal from './components/Modal';
+import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className='w-full flex items-center justify-center'>
-      <h1 className='text-5xl font-extrabold'>⚡️ Vite React Demo</h1>
-      <Modal />
+    <div className="w-full flex items-center justify-center">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div>Logo</div>
+      <nav>Nav</nav>
+      <div>Social</div>
+    </header>
+  );
+}
